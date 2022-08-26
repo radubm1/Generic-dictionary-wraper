8cef1bd72eba824d33fff8d781af4a1190f11cfe
Radu <Radu@DESKTOP-V9TMDUL>
2022-08-26 10:12:41 +0300

    Add project files.

---
+++ ConsoleApp1/Map.cs
@@ -0,0 +1,15 @@
+﻿class Map<K,V>{
+    public ISet<K> keys { set; get; }
+    public IList<V> values { set; get; }
+
+    public IDictionary<K,V> myDict()
+    {
+        IDictionary<K, V> dict = new Dictionary<K, V>();
+        int i = 0;
+        foreach(K key in keys)
+            dict.Add(key, values[i++]); 
+
+        return dict;  
+    }
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿class Test {
+    public static void Main(String[] args)
+    {
+        IList<string> my_lst = new List<string>();
+        foreach (String s in args)
+            my_lst.Add(s);
+
+        ISet<int> my_set = new HashSet<int>();
+        my_set.Add(1); my_set.Add(2); my_set.Add(3);
+
+        Map<int, string> my_dict = new Map<int, string>() { keys = my_set, values = my_lst };
+
+        IDictionary<int, string> dict = my_dict.myDict();
+
+        foreach (var pair in dict)
+        {
+            Console.WriteLine($"Element with key {pair.Key}: Id={pair.Value}");
+        }
+    }
+};
